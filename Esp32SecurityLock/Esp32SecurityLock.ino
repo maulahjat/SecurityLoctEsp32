69579aa2a5e50e9474b64fc1bc38d91fe34d4b44
maulahjat <59642654+maulahjat@users.noreply.github.com>
2022-12-01 16:02:49 +0500

    Basic Relay Code
    It will turn 5volt relay on and off which effects the solenoid lock

--- Esp32SecurityLock/Esp32SecurityLock.ino
+++ Esp32SecurityLock/Esp32SecurityLock.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#define ESP32_LED_BUILTIN 2
 
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(ESP32_LED_BUILTIN, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ESP32_LED_BUILTIN, HIGH);   // turn the Relay on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(ESP32_LED_BUILTIN, LOW);    // turn the Relay off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
